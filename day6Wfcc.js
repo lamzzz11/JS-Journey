066363a8e0995b4207ccad5a562f09b7032c881a
Ahmad Darussalam <ahmadbulian07@gmail.com>
2026-09-23 00:04:09 +0700

    belajar lagi

---
+++ day6Wfcc.js
@@ -0,0 +1,26 @@
+console.log(3);
+console.log(5);
+console.log(-67);
+// Examples of floating point numbers
+console.log(3.14);
+console.log(7.2);
+console.log(-14.5);
+
+console.log("I love to code!");
+
+let symbol = "💻";
+console.log(symbol);
+
+const nama = "John Doe";
+let age ;
+age = 26;
+console.log(`My name is ${nama} and I am ${age} years old.`);
+
+let _score = 100;
+let $balance = 250.75;
+console.log(`Score: ${_score}, Balance: $${$balance}`);
+
+const maxScore = 200;
+console.log(`Max Score: ${maxScore}`);
+
+// Belajar dari freecode camp.org (introduction to JavaScript)
